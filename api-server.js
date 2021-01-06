cae7dcd97e10e689ac1c6f70e74a74869130d77d
Dori <58356939+Betterslash@users.noreply.github.com>
2021-01-06 16:45:23 +0200

    Add files via upload

---
+++ api-server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json({limit: '10mb'}));
+app.options('*',cors());
+app.all("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+});
+
+app.post('/login', function (req, res) {
+    console.log(req.body);
+    res.send(req.body)
+  })
+  
+  app.listen(2021, function () {
+    console.log('Nodejs app listening on port 2021')
+  })
